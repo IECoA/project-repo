671117441f7c00476ee5d04a3ccc856b69d1b273
IEIndi <eivanov@indiprint.ru>
2019-11-01 16:36:03 +0300

    form and stuff

--- js/m-form.js
+++ js/m-form.js
@@ -8,6 +8,9 @@ function showPassword() {
   }
 }
 
+// Phone mask
+$('#phone').mask('+7 (000) 000-00-00');
+
 /* Change Placeholder jQuery */
 /*
 $('input[type=email]').focus(function() {
@@ -24,6 +27,8 @@ $('input[type=password]').focus(function() {
 
 
 // Form Tabs
+
+/*
 
 //if (window.matchMedia("(max-width: 560px)").matches) {
 var currentTab = 0; // Current tab is set to be the first tab (0)
@@ -69,3 +74,49 @@ function tabReset() {
   x[0].style.display = "block";
 }
 */
+
+
+// jQuery Form Tabs + Validation
+if (window.matchMedia("(max-width: 560px)").matches) {
+$(function () {
+  var $sections = $('.l-tab');
+
+  function navigateTo(index) {
+    // Mark the current section with the class 'current'
+    $sections
+      .removeClass('current')
+      .eq(index)
+        .addClass('current');
+    // Show only the navigation buttons that make sense for the current section:
+    $('#prevBtn').toggle(index > 0);
+    var atTheEnd = index >= $sections.length - 1;
+    $('#nextBtn').toggle(!atTheEnd);
+    $('.m-button[type=submit]').toggle(atTheEnd);
+  }
+
+  function curIndex() {
+    // Return the current index by looking at which section has the class 'current'
+    return $sections.index($sections.filter('.current'));
+  }
+
+  // Previous button is easy, just go back
+  $('#prevBtn').click(function() {
+    navigateTo(curIndex() - 1);
+  });
+
+  // Next button goes forward iff current block validates
+  $('#nextBtn').click(function() {
+    $('#l-vypuskForm').parsley().whenValidate({
+      group: 'block-' + curIndex()
+    }).done(function() {
+      navigateTo(curIndex() + 1);
+    });
+  });
+
+  // Prepare sections by setting the `data-parsley-group` attribute to 'block-0', 'block-1', etc.
+  $sections.each(function(index, section) {
+    $(section).find(':input').attr('data-parsley-group', 'block-' + index);
+  });
+  navigateTo(0); // Start at the beginning
+});
+}
