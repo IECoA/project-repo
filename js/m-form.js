14fbf6d2d7c31c71984d0d4712bc9910abd60b58
IEIndi <eivanov@indiprint.ru>
2019-11-08 18:49:03 +0300

    phone validation and js

--- js/m-form.js
+++ js/m-form.js
@@ -12,9 +12,63 @@ function showPassword() {
 $('#phone').mask('+7 (000) 000-00-00');
 
 //demask?
+/*
 $('#phone').parsley().on('field:validate', function() {
   $('#phone').cleanVal();
+  $('#phone').unmask();
+  console.log('Validation failed for: ', this.$element);
 });
+$(function() {
+  $('#l-vypuskForm').parsley().on('field:validated', function() {
+
+  }
+}*/
+
+// jQuery Form Tabs + Validation
+if (window.matchMedia("(max-width: 560px)").matches) {
+$(function () {
+  var $sections = $('.l-tab');
+
+  function navigateTo(index) {
+    // Mark the current section with the class 'current'
+    $sections
+      .removeClass('current')
+      .eq(index)
+        .addClass('current');
+    // Show only the navigation buttons that make sense for the current section:
+    $('#prevBtn').toggle(index > 0);
+    var atTheEnd = index >= $sections.length - 1;
+    $('#nextBtn').toggle(!atTheEnd);
+    $('.m-button[type=submit]').toggle(atTheEnd);
+  }
+
+  function curIndex() {
+    // Return the current index by looking at which section has the class 'current'
+    return $sections.index($sections.filter('.current'));
+  }
+
+  // Previous button is easy, just go back
+  $('#prevBtn').click(function() {
+    navigateTo(curIndex() - 1);
+  });
+
+  // Next button goes forward iff current block validates
+  $('#nextBtn').click(function() {
+    $('#l-vypuskForm').parsley().whenValidate({
+      group: 'block-' + curIndex()
+    }).done(function() {
+      navigateTo(curIndex() + 1);
+    });
+  });
+
+  // Prepare sections by setting the `data-parsley-group` attribute to 'block-0', 'block-1', etc.
+  $sections.each(function(index, section) {
+    $(section).find(':input').attr('data-parsley-group', 'block-' + index);
+  });
+  navigateTo(0); // Start at the beginning
+});
+}
+
 
 /* Change Placeholder jQuery */
 /*
@@ -79,49 +133,3 @@ function tabReset() {
   x[0].style.display = "block";
 }
 */
-
-
-// jQuery Form Tabs + Validation
-if (window.matchMedia("(max-width: 560px)").matches) {
-$(function () {
-  var $sections = $('.l-tab');
-
-  function navigateTo(index) {
-    // Mark the current section with the class 'current'
-    $sections
-      .removeClass('current')
-      .eq(index)
-        .addClass('current');
-    // Show only the navigation buttons that make sense for the current section:
-    $('#prevBtn').toggle(index > 0);
-    var atTheEnd = index >= $sections.length - 1;
-    $('#nextBtn').toggle(!atTheEnd);
-    $('.m-button[type=submit]').toggle(atTheEnd);
-  }
-
-  function curIndex() {
-    // Return the current index by looking at which section has the class 'current'
-    return $sections.index($sections.filter('.current'));
-  }
-
-  // Previous button is easy, just go back
-  $('#prevBtn').click(function() {
-    navigateTo(curIndex() - 1);
-  });
-
-  // Next button goes forward iff current block validates
-  $('#nextBtn').click(function() {
-    $('#l-vypuskForm').parsley().whenValidate({
-      group: 'block-' + curIndex()
-    }).done(function() {
-      navigateTo(curIndex() + 1);
-    });
-  });
-
-  // Prepare sections by setting the `data-parsley-group` attribute to 'block-0', 'block-1', etc.
-  $sections.each(function(index, section) {
-    $(section).find(':input').attr('data-parsley-group', 'block-' + index);
-  });
-  navigateTo(0); // Start at the beginning
-});
-}
