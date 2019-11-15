4e95bc990c82edc8958826827ad94a34d51de9e0
IEIndi <eivanov@indiprint.ru>
2019-11-15 17:14:01 +0300

    form js added and fix

---
+++ js/m-forms.js
@@ -0,0 +1,8 @@
+// Show Password Icon
+function showPassword() {
+var xB = document.getElementById("password");
+if (xB.type === "password") {
+  xB.type = "text";
+} else {
+  xB.type = "password";
+}}
