69ea94db795c32f1d30ca87bb6b3c5a9ade4751a
Evgenii <eivanov@indiprint.ru>
2020-10-06 18:10:59 +0300

    timer added

--- js/_sale.js
+++ js/_sale.js
@@ -27,4 +27,21 @@ document.getElementById("tempButton").onclick = gtmSaleSwitcher;
 
 function gtmSaleCountdown() {
     var countDownTime = new Date("Oct 12, 2021 15:30:25")
+    var x = setInterval(function () {
+        var now = new Date().getTime();
+        var timeLeft = countDownTime - now;
+        var daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 *24));
+        var hoursLeft = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutesLeft = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var secondsLeft = Math.floor((distance % (1000 * 60)) / 1000);
+        
+        document.getElementById("gtmTimeLeft").innerHTML = daysLeft + "дней" + hoursLeft + "часов" + minutesLeft + "минут" + secondsLeft + "секунд";
+        /*
+        if (daysLeft < 1) {
+            clearInterval(x);
+            document.getElementById("gtmTimeLeft").innerHTML = "Конец"
+        }
+        */
+
+    }, 1000);
 }
