fcab9ea7a19e82590fbece18845952178605c7d7
Evgenii <ivanov2a2005@yandex.ru>
2020-10-06 23:11:09 +0300

    cookie settings

--- js/_sale.js
+++ js/_sale.js
@@ -12,12 +12,31 @@ document.getElementById("gtm-close-icon").onclick = function() {
 */
 
 
-function gtmSaleSwitcher(){
-    var x = document.getElementById("sale-info");
-    if (x.style.display === "none") {
-        x.style.display = "flex";
+var gtmSaleElement = document.getElementById("sale-info");
+
+
+//Set Banner Visibility onDomLoad
+document.addEventListener("DOMContentLoaded", () => {
+    gtmSaleElement.style.display = "none";
+    x = document.cookie.valueOf("gtmSaleStatus");
+    cooValue = x.split("=");
+    console.log(cooValue[1]);
+    if (cooValue[1] != "hidden") {
+        gtmSaleElement.style.display = "flex";
     } else {
-        x.style.display = "none";
+        gtmSaleElement.style.display = "none";
+    }
+});
+
+
+// Свитч отображения
+function gtmSaleSwitcher(){
+    if (gtmSaleElement.style.display === "none") {
+        gtmSaleElement.style.display = "flex";
+        document.cookie = "gtmSaleStatus=visible;path=/;sameSite=Lax;Secure;expires=Tue, 19 Jan 2038";
+    } else {
+        gtmSaleElement.style.display = "none";
+        document.cookie = "gtmSaleStatus=hidden;path=/;sameSite=Lax;Secure;expires=Tue, 19 Jan 2038";
     }
 }
 
@@ -25,23 +44,20 @@ document.getElementById("gtm-close-icon").onclick = gtmSaleSwitcher;
 document.getElementById("tempButton").onclick = gtmSaleSwitcher;
 
 
-function gtmSaleCountdown() {
-    var countDownTime = new Date("Oct 12, 2021 15:30:25")
-    var x = setInterval(function () {
-        var now = new Date().getTime();
-        var timeLeft = countDownTime - now;
-        var daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 *24));
-        var hoursLeft = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutesLeft = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var secondsLeft = Math.floor((distance % (1000 * 60)) / 1000);
-        
-        document.getElementById("gtmTimeLeft").innerHTML = daysLeft + "дней" + hoursLeft + "часов" + minutesLeft + "минут" + secondsLeft + "секунд";
-        /*
-        if (daysLeft < 1) {
-            clearInterval(x);
-            document.getElementById("gtmTimeLeft").innerHTML = "Конец"
-        }
-        */
+// Sale Counter
+function gtmSaleCountdown(endTime) {
+    var countDownTime = new Date(endTime);
+    var y = setInterval(() => {
+            var now = new Date().getTime();
+            var timeLeft = countDownTime - now;
+            var daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+            var hoursLeft = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutesLeft = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+            var secondsLeft = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-    }, 1000);
+            document.getElementById("gtmTimeLeft").innerHTML = daysLeft + " дн. " + hoursLeft + " ч. " + minutesLeft + " мин. " + secondsLeft + " сек. ";
+
+        }, 1000);
 }
+
+gtmSaleCountdown("Oct 16, 2020 11:00");
