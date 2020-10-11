45612c4f0905d2617b1d457feba15012cf7caf4e
Evgenii <ivanov2a2005@yandex.ru>
2020-10-11 22:12:36 +0300

    added propogation, css

--- js/gtm-sale.js
+++ js/gtm-sale.js
@@ -5,63 +5,29 @@ document.body.insertAdjacentHTML('beforeend', saleHtml);
 document.getElementById('bs1').insertAdjacentHTML('beforeend', saleHtml);
 */
 
-/*
-document.getElementById("gtm-close-icon").onclick = function() {
-    document.getElementById("sale-info").style.visibility = "none";
-}
-*/
 
 
-const gtmSaleElement = document.getElementById("sale-info");
+let gtmSaleElement = document.getElementById("sale-info");
 
 // Set Banner Visibility onLoad and show after X
+
 document.addEventListener("DOMContentLoaded", () => {
     let x = document.cookie.replace(/(?:(?:^|.*;\s*)gtmSaleStatus\s*\=\s*([^;]*).*$)|^.*$/, "$1");
     console.log(x);
     switch (x) {
         case "visible":
-            gtmSaleElement.style.visibility = "visible";
-            gtmSaleElement.style.bottom = "0";
+            gtmSaleElement.className = "gtm-sale-container"
             break;
         
-        case "hidden":
-            gtmSaleElement.style.visibility = "hidden";
-            gtmSaleElement.style.bottom = "-230px";
-            break;
         default:
-            gtmSaleElement.style.visibility = "hidden";
-            gtmSaleElement.style.bottom = "-230px";
+            gtmSaleElement.className = "gtm-sale-container closed"
             setTimeout(() => {
-                gtmSaleElement.style.visibility = "visible";
-                gtmSaleElement.style.bottom = "0";
+                gtmSaleElement.classList.remove("closed");
                 document.cookie = "gtmSaleStatus=visible;path=/;expires=Tue, 19 Oct 2020 23:59:59 GMT";
             }, 2000);
             break;
     }
 });
-
-// Свитч отображения через switch
-function gtmSaleSwitch() {
-    switch (gtmSaleElement.style.visibility) {
-        case "hidden":
-            gtmSaleElement.style.visibility = "visible";
-            gtmSaleElement.style.bottom = "0";
-            document.cookie = "gtmSaleStatus=visible;path=/;expires=Tue, 19 Oct 2020 23:59:59 GMT";
-            break;
-        case "visible":
-            gtmSaleElement.style.visibility = "hidden";
-            gtmSaleElement.style.bottom = "-230px";
-            document.cookie = "gtmSaleStatus=hidden;path=/;expires=Tue, 19 Oct 2020 23:59:59 GMT";
-            break;
-        default:
-            break;
-    } 
-}
-
-// Вызов свитча по элементам
-document.getElementById("gtm-close-icon").onclick = gtmSaleSwitch;
-document.getElementById("tempButton").onclick = gtmSaleSwitch;
-
 
 // Sale Counter
 function gtmSaleCountdown(endTime) {
@@ -79,3 +45,61 @@ function gtmSaleCountdown(endTime) {
 }
 
 gtmSaleCountdown("Oct 16, 2020 11:00");
+
+
+// Свитч - смена класса
+
+function gtmSaleSwitch() {
+    let saleElemClasses = gtmSaleElement.classList;
+    switch (Object.values(saleElemClasses).includes("closed")) {
+        case true:
+            saleElemClasses.remove("closed");
+            console.log("Open executed on " + saleElemClasses);
+            document.cookie = "gtmSaleStatus=visible;path=/;expires=Tue, 19 Oct 2020 23:59:59 GMT";
+            break;
+        default:
+            saleElemClasses.add("closed");
+            console.log("Close executed on " + saleElemClasses);
+            document.cookie = "gtmSaleStatus=hidden;path=/;expires=Tue, 19 Oct 2020 23:59:59 GMT";
+            break;
+    }
+}
+
+document.getElementById("tempButton").onclick = gtmSaleSwitch;
+document.getElementById("sale-info").onclick = () => {
+    if (Object.values(gtmSaleElement.classList).includes("closed") === true) {
+        gtmSaleSwitch();
+    }
+};
+document.getElementById("gtm-close-icon").onclick = (event) => {
+        gtmSaleSwitch();
+        event.stopPropagation();
+}
+
+
+
+
+// ____Итерация 1____
+
+// Свитч отображения через switch
+/*
+function gtmSaleSwitch() {
+    switch (gtmSaleElement.style.visibility) {
+        case "hidden":
+            gtmSaleElement.style.visibility = "visible";
+            gtmSaleElement.style.bottom = "0";
+            document.cookie = "gtmSaleStatus=visible;path=/;expires=Tue, 19 Oct 2020 23:59:59 GMT";
+            break;
+        case "visible":
+            gtmSaleElement.style.visibility = "hidden";
+            gtmSaleElement.style.bottom = "-230px";
+            document.cookie = "gtmSaleStatus=hidden;path=/;expires=Tue, 19 Oct 2020 23:59:59 GMT";
+            break;
+        default:
+            break;
+    } 
+}
+*/
+
+
+
